760913888ab4466e1f9518a3bb06f6a2bf1c291b
Karthick Bharathwaj G <karthickbharathwaj@gmail.com>
2023-01-13 15:53:30 +0530

    Day 05 Changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,41 @@
+import { useState } from "react";
 import { restaurantList } from "../../constants";
 import RestaurantCard from "./RestaurantCard";
 
 function Body() {
+  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [searchText, setSearchText] = useState("");
+
+  function filterData(searchText, restaurants) {
+    const filterData = restaurants.filter((restaurant) => {
+      return restaurant.data.name.includes(searchText);
+    });
+
+    return filterData;
+  }
   return (
     <div className="restaurant-body">
       <div>
-        <input type="text" />
-        <button>search</button>
+        <input
+          value={searchText}
+          type="text"
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            const data = filterData(searchText, restaurantList);
+            setRestaurants(data);
+          }}
+        >
+          search
+        </button>
+        <div>{searchText}</div>
       </div>
 
       <div className="restaurant-list">
-        {restaurantList.map((ele) => {
-          console.log(ele.data);
+        {restaurants.map((ele) => {
           return <RestaurantCard {...ele.data} key={ele.data.id} />;
         })}
       </div>
